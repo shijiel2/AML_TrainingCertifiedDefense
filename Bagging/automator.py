9885c47a29535a0e85debea5058262984043b651
Shijie Liu <shijiel2@student.unimelb.edu.au>
2021-05-09 17:45:10 +1000

    update automator

--- Bagging/automator.py
+++ Bagging/automator.py
@@ -5,18 +5,18 @@ import socket
 train_command = 'python train.py --k {k} --end {base_num} --poison_size {ps}'
 certify_command = 'python certify.py --k {k} --ns {base_num} --poison_size {ps}'
 
-k = 50
+k_list = [100, 500, 1000, 5000]
 base_num = 1000
-
-ps_list = [0, 5000, 10000, 20000]
+ps_list = [0]
 
 for ps in ps_list:
-    proc = Popen(train_command.format(k=k, base_num=base_num, ps=ps),
-                 shell=True,
-                 cwd='./')
-    proc.wait()
-    proc = Popen(certify_command.format(k=k, base_num=base_num, ps=ps),
-                 shell=True,
-                 cwd='./')
-    proc.wait()
+    for k in k_list:
+        proc = Popen(train_command.format(k=k, base_num=base_num, ps=ps),
+                    shell=True,
+                    cwd='./')
+        proc.wait()
+        proc = Popen(certify_command.format(k=k, base_num=base_num, ps=ps),
+                    shell=True,
+                    cwd='./')
+        proc.wait()
 NOTIFIER.notify(socket.gethostname(), 'Done.')
